988a5f04a4ff2c82bdc2c488cfdc033ce444b8d2
daviWal <david.walitza@icloud.com>
2022-05-17 17:31:57 +0200

    bugfixes

--- DerMarcl_daviWal_android/Dummy.java
+++ DerMarcl_daviWal_android/Dummy.java
@@ -2,7 +2,12 @@ public class Dummy {
 
     public void thisIsANewMethod() {
 	System.out.println("added by member 1");        
+	//Bugfix1
         System.out.println("Added by Member2");
     }
+
+//bugfix2
+//anotherbugFix
+
 static int NUMBER = 10;
 }
